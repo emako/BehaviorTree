6c17c6a2405b51439e08140dc03924826bc158cc
ema <mccoy39082@163.com>
2026-06-04 16:28:33 +0800

    Simplify nuget_pack.ps1 to run from src

--- build/nuget_pack.ps1
+++ build/nuget_pack.ps1
@@ -9,18 +9,11 @@ Write-Host @"
 ╚═╝  ╚═══╝ ╚═════╝  ╚═════╝ ╚══════╝   ╚═╝   
 "@
 
-$projects = @(
-    "..\src\BehaviorTree"
-)
-
-foreach ($proj in $projects) {
-    Push-Location $proj
-    Write-Host "Processing $proj..."
-    dotnet restore
-    dotnet build -c Release
-    dotnet pack -c Release -o ../build/
-    Pop-Location
-}
+Push-Location ..\src\
+dotnet restore
+dotnet build -c Release
+dotnet pack -c Release -o ../build/
+Pop-Location
 
 Write-Host "`nPress any key to exit..."
 [void][System.Console]::ReadKey($true)
